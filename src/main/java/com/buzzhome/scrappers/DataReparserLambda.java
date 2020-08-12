508bd70a31438dafce3d672953594fede836fcee
Willson Ngo <wngo@grubhub.com>
2020-08-11 22:56:56 -0400

    fix DataReparserLambda

--- src/main/java/com/buzzhome/scrappers/DataReparserLambda.java
+++ src/main/java/com/buzzhome/scrappers/DataReparserLambda.java
@@ -15,8 +15,14 @@ import java.util.Optional;
 @Slf4j
 public class DataReparserLambda implements RequestHandler<IntegerRequest, String> {
 
+    private static final double EPSILON = 0.00001;
+
+    private boolean compareDouble(double first, double second) {
+        return Math.abs(first - second) < EPSILON;
+    }
+
     public String handleRequest(IntegerRequest request, Context context) {
-        log.info("DataReparserLamba getting request to reparse data in the last {} days", request);
+        log.info("DataReparserLamba getting request to reparse data in the last {} days", request.getValue());
         List<FbGroupContent> fbGroupContent = DynamodbClient.getPosts(request.getValue());
 
         for (FbGroupContent post : fbGroupContent) {
@@ -25,17 +31,17 @@ public class DataReparserLambda implements RequestHandler<IntegerRequest, String
             double price = PriceDataParser.getPriceInUSD(post.getContent());
 
             if ((!districtLocation.isPresent() && post.getDistrictLocation() != null)
-                    || (districtLocation.isPresent() && districtLocation.get() != post.getDistrictLocation())
-                    || price != post.getPrice()) {
+                    || (districtLocation.isPresent() && !districtLocation.get().equals(post.getDistrictLocation()))
+                    || !compareDouble(price, post.getPrice())) {
                 if (districtLocation.isPresent()) {
                     post.setDistrictLocation(districtLocation.get());
                 } else {
                     post.setDistrictLocation(null);
                 }
+                post.setPrice(price);
 
                 log.info("Updating record with hashkey={}, rangekey={}: districtLocation={}, price={}",
                         post.getId(), post.getPostedTimestamp(), districtLocation, price);
-                post.setPrice(price);
                 DynamodbClient.update(post);
             }
         }
